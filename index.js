497bc420d74c921f00e1d8c283efbc56bcf814fe
BoRoDaTeNkiY <polpoltrop228@gmail.com>
2024-03-07 12:00:00 +0200

    Stream Key

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const NodeMediaServer = require("node-media-server");
 const { ApolloServer } = require("apollo-server");
-const { config } = require('./mediaServer/config');
 const mongoose = require("mongoose");
+const node_media_server = require('./media_server');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const MONGODB_KEY = process.env.MONGODB_KEY;
-
-const nms = new NodeMediaServer(config);
-nms.run();
 
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
@@ -21,6 +18,7 @@ const server = new ApolloServer({
 mongoose.connect(MONGODB_KEY, { useNewUrlParser: true })
     .then(() => {
         console.log("MongoDB connection successful");
+        node_media_server.run();
         return server.listen({port: 3000});
     })
     .then((res) => {
