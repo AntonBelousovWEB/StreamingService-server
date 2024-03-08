b7503756fa1140c88013c20fcd6c7ba905a3f728
BoRoDaTeNkiY <polpoltrop228@gmail.com>
2024-03-08 12:00:00 +0200

    Fix

--- media_server.js
+++ media_server.js
@@ -6,16 +6,18 @@ const nms = new NodeMediaServer(config);
 
 nms.on('prePublish', async (id, StreamPath, args) => {
     let stream_key = getStreamKeyFromStreamPath(StreamPath);
-    console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-
+    console.log('[NodeEvent on prePublish]', 
+                `id=${id} StreamPath=${StreamPath} 
+                args=${JSON.stringify(args)}`);
+  
     try {
-        const user = await User.findOne({ stream_key: stream_key }).exec();
-        if (!user) {
-            let session = nms.getSession(id);
-            session.reject();
-        }
+      const user = await User.findOne({ streamKey: stream_key });
+      if (!user) {
+        let session = nms.getSession(id);
+        session.reject();
+      }
     } catch (err) {
-        console.error("Error while querying user:", err);
+      console.error("Error finding user:", err);
     }
 });
 
